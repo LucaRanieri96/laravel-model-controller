2c57120a5768e38f982fb704be677c3073a1cf4d
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-24 16:38:21 +0200

    Bonus Add style and images to database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,32 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="container">
-        <div class="row row-cols-3">
-            @forelse ($movies as $movie)
-                <div class="col g-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <h3 class="card-title">{{ $movie->title }}</h3>
-                            <p>{{ $movie->original_title }}</p>
-                            <p>{{ $movie->nationality }}</p>
-                            <p>{{ $movie->date }}</p>
-                            <p>{{ $movie->vote }}</p>
-                        </div>
-                    </div>
-                </div>
-            @empty
-                <p>No movie found</p>
-
-            @endforelse
+  <div class="container">
+    <h1 class="text-center my-5">Movies Database</h1>
+    <div class="row row-cols-1 row-cols-lg-2 my-5">
+      @forelse ($movies as $movie)
+      <div class="col g-3">
+        <div class="card">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="{{ $movie->image_url }}" class="img-fluid rounded-start" alt="{{ $movie->title }}">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">{{ $movie->title }}</h5>
+                <p class="card-text">Original title: "{{ $movie->original_title }}"</p>
+                <p class="card-text"><small class="text-body-secondary">nationality: {{ $movie->nationality }}</small></p>
+                <p class="card-text"><small class="text-body-secondary">date:{{ $movie->date }}</small></p>
+                <p class="card-text"><small class="text-body-secondary">vote: {{ $movie->vote }}</small></p>
+              </div>
+            </div>
+          </div>
         </div>
-
+      </div>
+      @empty
+      <p>No movie found</p>
+      @endforelse
     </div>
+
+  </div>
 @endsection
