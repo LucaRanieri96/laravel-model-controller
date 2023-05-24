dc12b260658649a229c1453ebf261b7cfd6f6f5a
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-24 14:56:06 +0200

    Update PageController and Route

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function movies() {
+        $movies = (Movie::all());
+        return view('welcome', compact('movies'));
+    }
 }
